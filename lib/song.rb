29aabd448375badd785462595a49c987cd70264a
Laila Campbell <lailacampbell96@gmail.com>
2020-03-15 03:51:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,13 +8,11 @@ class Song
   @@all << self 
  end
  
- def self.all 
- end  
+ def self.all
+   @@all
+end  
  
  def artist_name #artist_name knows the name of its artist if not returns nil
-    # if self.artist == nil 
-    # else             
-    #   self.artist.name 
    self.artist ? self.artist.name : nil #2nd option with ternary op
  end 
   

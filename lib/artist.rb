09840f8f78cad6ba92b607c50469aeb1f1dd8284
Laila Campbell <lailacampbell96@gmail.com>
2020-03-15 03:41:37 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,16 @@
+require 'pry'
+
+class Artist
+  
+attr_accessor :name
+  
+  def initialize (name)
+    @@name = name
+    @name = name
+  end
+  
+  def songs 
+    artist = []
+  end 
+      
+end 

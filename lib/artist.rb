1863d3ca229e14828b2e985bc07479d0e188d49b
Laila Campbell <lailacampbell96@gmail.com>
2020-03-15 03:48:17 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -10,7 +10,23 @@ attr_accessor :name
   end
   
   def songs 
-
+ Song.all.select do |song|
+     song.artist == self 
+    end
+   end
+   
+   def add_song(song) artist
+      song.artist = self  Song
+  end
+  
+  def add_song_by_name(song_name) 
+    new_song = Song.new(song_name) 
+    add_song(new_song) #and pass it in our add_song method,which adds a song to an artist's collection
+  end 
+  
+  def self.song_count #returns a total num of songs
+    Song.all.count
+  end
   
       
 end 
